1e163669e9ab1f815d740a3b3b150cc3982516ef
Benjamin Wong <tszkan.wong@gmail.com>
2020-12-09 12:01:24 +0800

    add message parser for etoro trading

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ from configparser import ConfigParser
 from dotenv import load_dotenv
 import os
 import logging
+from re import sub, search
+from decimal import Decimal
 
 load_dotenv()
 
@@ -15,7 +17,16 @@ configur.read('config.ini')
 
 forwards = configur.sections()
 
-
+def parse_message(message: str) -> tuple:
+    """docstring for parse_message"""
+    '0811 !新華文軒 買入訊號 @4.98(1.4%) 成交額 $4.3M 目標價 $5.24 止蝕價 $4.72 目標升幅 5.2% 止損 5.2% 一手 $4980 命中率 2020 0/1, 2019 1/1, 歷史 16/24(67%) [9/12/2020]'
+    
+    _splits = message.split(' ')
+    instrument = _splits[0]
+    tp = Decimal( sub(r'[^\d.]', '', _splits[_splits.index('目標價') + 1]) )
+    sl = Decimal( sub(r'[^\d.]', '', _splits[_splits.index('止蝕價') + 1]) )
+    signal_date = search(r'\[.*\]', message).group(0)
+    return instrument, tp, sl, signal_date
 
 
 def get_forward(forward: str) -> tuple:
